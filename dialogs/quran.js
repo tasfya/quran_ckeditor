b6a3cf4854fb16541761f61aa3f4b07aaeea6828
Mohamed El Hachimi <mohamed.rez@gmail.com>
2015-07-18 18:14:19 +0100

    added the ajax call that loads quran.xml

--- dialogs/quran.js
+++ dialogs/quran.js
@@ -16,5 +16,32 @@ CKEDITOR.dialog.add( 'quranDialog', function ( editor ) {
                   }]
       },
     ],
+    onOk: function() {
+      var dialog = this;
+      var abbr = editor.document.createElement( 'abbr' );
+
+      // abbr.setAttribute( 'title', dialog.getValueOf( 'tab-basic', 'title' ) );
+      // abbr.setText( dialog.getValueOf( 'tab-basic', 'abbr' ) );
+
+      // var id = dialog.getValueOf( 'tab-adv', 'id' );
+      // if ( id )
+      //     abbr.setAttribute( 'id', id );
+
+      // editor.insertElement( abbr );
+      var ayate = dialog.getValueOf( 'tab-basic', 'quran' );
+      $.ajax({
+        url: '/bower_components/ckeditor/plugins/quran/quran.xml',
+      })
+      .done(function(data) {
+        console.log($(data).find( "[index='1']" ));
+      })
+      .fail(function(error) {
+        console.log(error);
+      })
+      .always(function() {
+        console.log("complete");
+      });
+
+    }
   };
 });
